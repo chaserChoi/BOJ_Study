b0dfd6749b5caa1a17f64f10c8fbdfe8840adf90
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-02-25 23:36:46 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 224 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T, A, B;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+
+	return 0;
+}
