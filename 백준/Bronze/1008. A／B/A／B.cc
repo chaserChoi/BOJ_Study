d7093c507cdf523df790e74fb0620c8cf8fea301
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-02-25 23:25:39 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a, b;
+	double result;
+
+	cin >> a >> b;
+	result = a / b;
+
+	cout.precision(10);
+	cout << result << endl;
+
+	return 0;
+}
