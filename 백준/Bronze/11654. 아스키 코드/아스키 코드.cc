46b8202f9f94648f866ba1338490b0590aab7ab9
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-02-25 23:36:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main() {
+	char ch;
+	int num;
+	cin >> ch;
+	num = (int)ch;
+
+	cout << num << "\n";
+
+	return 0;
+}
