abe58eccd3eff6bb20d85b25d4e936949eceff4e
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-02-28 03:19:23 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int young, middle, old;
+    cin >> young >> middle;
+    old = middle + (middle - young);
+    cout << old << "\n";
+    return 0;
+}
