5b72aae8c808c46540f85fda8efa2eb271f370c6
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-03-04 23:26:32 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long N, M;
+    cin >> N >> M;
+    if (N == M) cout << 1 << "\n";
+    else cout << 0 << "\n";
+    return 0;
+}
