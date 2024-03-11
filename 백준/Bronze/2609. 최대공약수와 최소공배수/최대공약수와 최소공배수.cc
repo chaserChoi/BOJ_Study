7429ea9484f579f2c6646fdcafe1d22675f9d9ad
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-03-12 01:47:57 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    int c = a % b;
+    while (c != 0) {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    return b;
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a, b);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << "\n" << lcm(a, b) << "\n";
+    return 0;
+}
