294c5009a6e35b7ecc1df578dc22c4d1d914c15a
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-03 23:00:43 +0900

    [Bronze V] Title: 사칙연산, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(A+B));
+        bw.newLine();
+        bw.write(String.valueOf(A-B));
+        bw.newLine();
+        bw.write(String.valueOf(A*B));
+        bw.newLine();
+        bw.write(String.valueOf(A/B));
+        bw.newLine();
+        bw.write(String.valueOf(A%B));
+        bw.newLine();
+        bw.flush();
+        bw.close();
+    }
+}
