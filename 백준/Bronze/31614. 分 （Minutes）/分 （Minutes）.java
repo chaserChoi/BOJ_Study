11eea239931f0c447d0fe4aca53f4116b7dea9cc
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-03 23:21:25 +0900

    [Bronze V] Title: 分 (Minutes), Time: 120 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int H = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(H*60+M));
+        bw.flush();
+        bw.close();
+    }
+}
