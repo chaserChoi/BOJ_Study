da9ef4de47b6401f5c3ef71bdecd5db7a38fc431
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-06 10:57:31 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 140 ms, Memory: 14592 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int Y = Integer.parseInt(br.readLine());
+
+        int bananas = 3;
+
+        int totalFruits = X + Y + bananas;
+
+        bw.write(totalFruits + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
