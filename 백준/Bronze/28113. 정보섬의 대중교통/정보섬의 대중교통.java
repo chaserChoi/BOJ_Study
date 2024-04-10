58619142a35513f925b4b9a79cf0b25c7fed53ad
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-10 22:41:23 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if(A < B) bw.write("Bus");
+        else if(A > B) bw.write("Subway");
+        else bw.write("Anything");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
