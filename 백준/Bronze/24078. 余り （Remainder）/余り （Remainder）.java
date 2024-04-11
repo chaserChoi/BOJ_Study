348a884875a79cf38629fe345320f6cfaae1c93d
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-11 20:50:00 +0900

    [Bronze V] Title: 余り (Remainder), Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+
+        int remainder = X % 21;
+
+        bw.write(String.valueOf(remainder));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
