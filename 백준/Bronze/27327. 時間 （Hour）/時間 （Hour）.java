6754953a0068858499297ae6bec5a0be7e01f0e4
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-11 20:37:55 +0900

    [Bronze V] Title: 時間 (Hour), Time: 120 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+
+        int hours = X * 24;
+
+        bw.write(String.valueOf(hours));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
