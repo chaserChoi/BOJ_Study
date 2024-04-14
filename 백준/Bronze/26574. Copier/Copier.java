7793c46d072324851714b91ef8ae4382c73704c1
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-14 10:33:43 +0900

    [Bronze V] Title: Copier, Time: 144 ms, Memory: 16084 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String number = br.readLine();
+            bw.write(number + " " + number + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
