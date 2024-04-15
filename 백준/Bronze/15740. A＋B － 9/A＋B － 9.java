5e05dd910269866d73d767108027ad0eb2e2d292
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-15 18:59:20 +0900

    [Bronze V] Title: A+B - 9, Time: 248 ms, Memory: 17336 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger result = A.add(B);
+
+        bw.write(result.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
