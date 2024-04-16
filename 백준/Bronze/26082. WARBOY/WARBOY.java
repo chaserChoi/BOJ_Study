aad4c275e63deceeaf7e1192360dc1419b7bc4e6
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-16 15:51:28 +0900

    [Bronze V] Title: WARBOY, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int warboyPerformance = (B / A) * C * 3;
+
+        bw.write(String.valueOf(warboyPerformance));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
