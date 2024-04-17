5dbcc2d11afc28a65150966c4b1f78d81e8a69b8
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-17 21:49:05 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        if (A == B + C || B == A + C || C == A + B) bw.write("1");
+        else bw.write("0");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
