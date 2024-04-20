c41878e4b784659c35afff51bbb31d204f0c7c7e
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-20 18:36:31 +0900

    [Bronze IV] Title: Area, Time: 124 ms, Memory: 14184 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/28490. Area/Area.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int area = h * w;
+            if (area > maxArea) maxArea = area;
+        }
+
+        bw.write(String.valueOf(maxArea));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
