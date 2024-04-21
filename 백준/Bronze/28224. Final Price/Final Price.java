033cc28084904e5c7a1a04406f2697fa112c2f75
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-21 13:45:46 +0900

    [Bronze IV] Title: Final Price, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/28224. Final Price/Final Price.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int price = Integer.parseInt(br.readLine());
+        for (int i = 1; i < n; i++) {
+            int change = Integer.parseInt(br.readLine());
+            price += change;
+        }
+
+        bw.write(String.valueOf(price));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
