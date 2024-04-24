041e522e4bceef5b226a434215ba9c5fa28bd55a
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-24 20:35:41 +0900

    [Bronze IV] Title: Absolutely, Time: 132 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/26500. Absolutely/Absolutely.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine()); 
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            double value1 = Double.parseDouble(st.nextToken());
+            double value2 = Double.parseDouble(st.nextToken());
+
+            double absoluteDistance = Math.abs(value1 - value2); 
+
+            bw.write(String.format("%.1f", absoluteDistance));
+            bw.newLine();
+        }
+
+        br.close(); 
+        bw.close(); 
+    }
+}
