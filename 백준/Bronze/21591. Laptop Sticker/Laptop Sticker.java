d3c4ceff5455ddaa6ce5c17f749dd25fda35d312
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-29 19:49:00 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int wc = Integer.parseInt(st.nextToken());
+        int hc = Integer.parseInt(st.nextToken());
+        int ws = Integer.parseInt(st.nextToken());
+        int hs = Integer.parseInt(st.nextToken());
+
+        if (ws + 2 <= wc && hs + 2 <= hc) bw.write("1");
+        else bw.write("0");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
