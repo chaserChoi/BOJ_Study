9aefca06d81be625e89e8204bf71ca36e93c4e78
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-29 19:43:39 +0900

    [Bronze IV] Title: Checkers, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/21631. Checkers/Checkers.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long maxStripes = (a < b) ? a + 1 : b;
+
+        bw.write(String.valueOf(maxStripes));
+        bw.newLine();
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
