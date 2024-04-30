dda22d766041bd3b70c646ec10fb9d0a10437529
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-04-30 22:31:33 +0900

    [Bronze IV] Title: Covid-19, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/20673. Covid－19/Covid－19.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int p = Integer.parseInt(br.readLine());
+        int q = Integer.parseInt(br.readLine());
+
+        if (p <= 50 && q <= 10) bw.write("White");
+        else if (q > 30) bw.write("Red");
+        else bw.write("Yellow");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
