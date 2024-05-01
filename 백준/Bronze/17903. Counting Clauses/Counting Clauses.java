f6896156f32fd7014d35f75f56e7ee6956f28f93
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-01 18:23:03 +0900

    [Bronze IV] Title: Counting Clauses, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] query = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                if (st.hasMoreTokens()) {
+                    query[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+        if (n >= 8) bw.write("satisfactory");
+        else bw.write("unsatisfactory");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
