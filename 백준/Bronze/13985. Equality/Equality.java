7bae1d0c013dd0d912861f8943f09378f1b0203c
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-04 22:35:26 +0900

    [Bronze IV] Title: Equality, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/13985. Equality/Equality.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        String[] parts = input.split(" ");
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[2]);
+        int c = Integer.parseInt(parts[4]);
+
+        if (a + b == c) bw.write("YES\n");
+        else bw.write("NO\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
