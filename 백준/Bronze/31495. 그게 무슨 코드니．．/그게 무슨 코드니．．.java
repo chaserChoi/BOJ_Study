eb22375a1da6aa524cf905cb100ff341be28df68
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-09 00:59:57 +0900

    [Bronze IV] Title: 그게 무슨 코드니.., Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/31495. 그게 무슨 코드니．．/그게 무슨 코드니．．.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main extends Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+        String answer = "";
+
+        char start = str.charAt(0);
+        char end = str.charAt(str.length()-1);
+
+        if (start == '"' && end == '"' && str.length() <= 2) answer = "CE";
+        else if(start == '"' && end == '"')answer = str.substring(1, str.length()-1);
+        else answer = "CE";
+
+        sb.append(answer);
+        System.out.println(sb.toString());
+    }
+}
