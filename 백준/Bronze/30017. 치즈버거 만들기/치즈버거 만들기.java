980e806eb28e916afefcacec0cf33457f867bca5
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-10 21:24:57 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] burger = new int[2];
+
+        for(int i = 0; i <= st.countTokens(); i++) burger[i] = Integer.parseInt(st.nextToken());
+
+        if(burger[0] <= burger[1]) System.out.println(burger[0]*2-1);
+        else System.out.println(burger[1]*2+1);
+    }
+}
+
