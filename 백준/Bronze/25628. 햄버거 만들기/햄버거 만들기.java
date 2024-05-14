fd7d02ab9d91e44a500bd3791fe5c37c9d6cfa26
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-14 15:36:58 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int maxBurgers = Math.min(A / 2, B);
+
+        bw.write(String.valueOf(maxBurgers));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
