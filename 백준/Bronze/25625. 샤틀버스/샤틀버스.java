ff3de03d5415179664663b5290c094e0f12543ba
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-14 15:53:48 +0900

    [Bronze IV] Title: 샤틀버스, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/25625. 샤틀버스/샤틀버스.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int waitTime;
+
+        if (y < x) waitTime = x + y;
+        else waitTime = y - x;
+
+        bw.write(String.valueOf(waitTime));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
