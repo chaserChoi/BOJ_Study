c07f9d23123b86f3d74e21ea9f90117509ca3ba1
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-15 16:22:19 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 136 ms, Memory: 15428 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        String S = br.readLine();
+
+        bw.write(S.substring(S.length() - 5));
+
+        bw.flush();
+        bw.close();
+        br.close(); 
+    }
+}
