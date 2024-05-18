e4b03d902ec996716de81de9267fc865c3cf764f
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-18 19:34:33 +0900

    [Bronze IV] Title: 모음의 개수, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String word = br.readLine();
+
+        int count = 0;
+
+        String vowels = "aeiou";
+
+        for (int i = 0; i < word.length(); i++) {
+            if (vowels.contains(String.valueOf(word.charAt(i)))) count++;
+        }
+
+        bw.write(String.valueOf(count));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
