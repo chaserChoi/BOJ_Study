c570dc57f8da4141604326409c08925161aa8cd3
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-20 17:48:01 +0900

    [Bronze IV] Title: R2, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int R2 = 2 * S - R1;
+
+        bw.write(String.valueOf(R2));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
