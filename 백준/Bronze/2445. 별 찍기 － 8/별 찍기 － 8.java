9f2240a5cca7de4091ff6e364b9951df779790db
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-23 14:08:49 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 144 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); 
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) bw.write("*");
+            for (int j = 0; j < 2 * (N - i); j++) bw.write(" ");
+            for (int j = 0; j < i; j++) bw.write("*");
+            bw.newLine();
+        }
+
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) bw.write("*");
+            for (int j = 0; j < 2 * (N - i); j++) bw.write(" ");
+            for (int j = 0; j < i; j++) bw.write("*");
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
