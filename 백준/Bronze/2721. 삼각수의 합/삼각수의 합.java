d7fdc63cf32f52e8f1fdc4f7b674338087d4afa9
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-05-25 12:53:38 +0900

    [Bronze III] Title: 삼각수의 합, Time: 148 ms, Memory: 14796 KB -BaekjoonHub

---
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            int sum = 0;
+            for(int k = 1; k <= n; k++) sum += k * (k + 1) * (k + 2) / 2;
+
+            bw.write(sum + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
