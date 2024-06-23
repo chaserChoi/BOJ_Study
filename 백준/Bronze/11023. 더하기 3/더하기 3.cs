bcb1a5e20794f5df92a9d5c397943ef3f7355696
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-06-23 17:55:03 +0900

    [Bronze III] Title: 더하기 3, Time: 92 ms, Memory: 5592 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (TextReader sr = new StreamReader(Console.OpenStandardInput()))
+        using (TextWriter sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false })
+        {
+            string[] input = sr.ReadLine().Split();
+            int sum = 0;
+
+            foreach (string s in input)
+                sum += int.Parse(s);
+
+            sw.WriteLine(sum);
+
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
