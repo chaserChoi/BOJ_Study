6be2acbfba5fd4770ea79734297bfef22801c1b2
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-07-12 15:15:07 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 56 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cs
@@ -0,0 +1,21 @@
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var sr = new StreamReader(Console.OpenStandardInput()))
+        using (var sw = new StreamWriter(Console.OpenStandardOutput()))
+        {
+            long n = long.Parse(sr.ReadLine());
+
+            sw.WriteLine(4 * n);
+
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
