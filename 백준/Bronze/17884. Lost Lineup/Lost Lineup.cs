d547de15304f5ae4d4c829a8bf936505b8d55ee8
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-07-18 12:23:49 +0900

    [Bronze III] Title: Lost Lineup, Time: 76 ms, Memory: 6088 KB -BaekjoonHub

---
+++ 백준/Bronze/17884. Lost Lineup/Lost Lineup.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var sr = new StreamReader(Console.OpenStandardInput()))
+        using (var sw = new StreamWriter(Console.OpenStandardOutput()))
+        {
+            int n = int.Parse(sr.ReadLine());
+            var v = new List<(int, int)>();
+
+            string[] line = sr.ReadLine().Split();
+            for (int i = 0; i < n - 1; i++)
+                v.Add((int.Parse(line[i]) , i));
+
+            v.Sort((a, b) => a.Item1.CompareTo(b.Item1));
+
+            sw.Write("1");
+            for (int i = 0; i < n - 1; i++)
+                sw.Write(" " + (v[i].Item2 + 2));
+            sw.WriteLine();
+
+            sw.Flush();
+            sw.Close();
+            sr.Close();
+        }
+    }
+}
