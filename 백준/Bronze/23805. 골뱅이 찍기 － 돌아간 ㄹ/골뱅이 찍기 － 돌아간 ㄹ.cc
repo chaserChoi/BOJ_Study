fb46c4c0388cad7f76bd0ef74aea7e22a3a7bb40
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-07-31 11:59:44 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㄹ, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23805. 골뱅이 찍기 － 돌아간 ㄹ/골뱅이 찍기 － 돌아간 ㄹ.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n * 3; j++) cout << "@";
+        for (int j = 0; j < n; j++) cout << " ";
+        for (int j = 0; j < n; j++) cout << "@";
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n * 3; i++) {
+        for (int j = 0; j < n; j++) cout << "@";
+        for (int j = 0; j < n; j++) cout << " ";
+        for (int j = 0; j < n; j++) cout << "@";
+        for (int j = 0; j < n; j++) cout << " ";
+        for (int j = 0; j < n; j++) cout << "@";
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cout << "@";
+        for (int j = 0; j < n; j++) cout << " ";
+        for (int j = 0; j < n * 3; j++) cout << "@";
+        cout << "\n";
+    }
+
+    return 0;
+}
