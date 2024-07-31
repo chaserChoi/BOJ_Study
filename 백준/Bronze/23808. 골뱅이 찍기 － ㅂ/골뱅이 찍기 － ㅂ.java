25274bfe5808df479b4f961e1f1167e35531a518
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-07-31 12:21:06 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 212 ms, Memory: 16116 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n * 2; i++) {
+            for (int j = 0; j < n; j++)
+                pw.print("@");
+            for (int j = 0; j < n * 3; j++)
+                pw.print(" ");
+            for (int j = 0; j < n; j++)
+                pw.print("@");
+            pw.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++)
+                pw.print("@");
+            pw.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                pw.print("@");
+            for (int j = 0; j < n * 3; j++)
+                pw.print(" ");
+            for (int j = 0; j < n; j++)
+                pw.print("@");
+            pw.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n * 5; j++)
+                pw.print("@");
+            pw.println();
+        }
+
+        pw.flush();
+        pw.close();
+        br.close();
+    }
+}
