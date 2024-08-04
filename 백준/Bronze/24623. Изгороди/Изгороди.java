77b5491625d4563da07188bc2a840d858d40ff60
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-08-04 13:32:57 +0900

    [Bronze III] Title: Изгороди, Time: 112 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/24623. Изгороди/Изгороди.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PrintWriter pw = new PrintWriter(System.out);
+
+        int n = Integer.parseInt(br.readLine());
+        int alpha = Integer.parseInt(br.readLine());
+
+        pw.println(90);
+
+        pw.flush();
+        pw.close();
+        br.close();
+    }
+}
