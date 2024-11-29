34d8e7ad851d4a1412118c03527124c13d714269
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2024-11-29 15:23:34 +0900

    [Bronze II] Title: 주사위 게임, Time: 112 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/5566. 주사위 게임/주사위 게임.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        int[] board = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+            board[i] = Integer.parseInt(br.readLine());
+
+        int[] dice = new int[m];
+        for (int i = 0; i < m; i++)
+            dice[i] = Integer.parseInt(br.readLine());
+
+        int currPos = 1;
+        for (int i = 0; i < m; i++) {
+            currPos += dice[i];
+
+            if (currPos >= n) {
+                System.out.println(i + 1);
+                return;
+            }
+
+            currPos += board[currPos];
+
+            if (currPos >= n) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+    }
+}
