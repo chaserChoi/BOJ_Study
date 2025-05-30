7ea6b755476ed29971694cf258d8ccd835268a93
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2025-05-30 18:01:45 +0900

    [Bronze II] Title: 이진수 변환, Time: 108 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/10829. 이진수 변환/이진수 변환.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long n = Long.parseLong(br.readLine());
+
+        String binary = Long.toBinaryString(n);
+
+        bw.write(binary);
+        bw.newLine();
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
