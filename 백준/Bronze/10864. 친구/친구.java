a58a246cf727a9ad3c79f6013f9c1139798dd147
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2025-06-02 17:32:42 +0900

    [Bronze II] Title: 친구, Time: 124 ms, Memory: 14556 KB -BaekjoonHub

---
+++ 백준/Bronze/10864. 친구/친구.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] students = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            students[a]++;
+            students[b]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            bw.write(String.valueOf(students[i]));
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
