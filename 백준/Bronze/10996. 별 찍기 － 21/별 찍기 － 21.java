e9df80a4a6b4b2c03e1f715ee76c4956708688e8
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2025-06-04 12:23:57 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 116 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= 2 * n; i++) {
+            if (i % 2 == 1) {
+                for (int j = 1; j <= n; j++) {
+                    if (j % 2 == 1)
+                        bw.write("*");
+                    else
+                        bw.write(" ");
+                }
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    if (j % 2 == 1)
+                        bw.write(" ");
+                    else
+                        bw.write("*");
+                }
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
