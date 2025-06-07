41bc384bc823eec2689de3450c76c2e6450cdab8
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2025-06-08 00:25:32 +0900

    [Bronze II] Title: 꿍 가라사대, Time: 116 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/11094. 꿍 가라사대/꿍 가라사대.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+
+            if (line.toLowerCase().startsWith("simon says")) {
+                String inst = line.substring(10);
+
+                bw.write(inst);
+                bw.newLine();
+            }
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
