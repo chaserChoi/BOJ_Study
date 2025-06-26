cd99d737e3fac9adb2476d1ebf15f9c078014f45
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2025-06-26 13:41:03 +0900

    [Silver V] Title: 색종이, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] paper = new boolean[100][100];
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    if (!paper[j][k]) {
+                        paper[j][k] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(sb.append(area));
+
+        br.close();
+    }
+}
