9737c62295c2038f2c3d1cf868cd8ab00c534ea6
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2026-01-15 01:43:47 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 221.33 ms, Memory: 49.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    
+    val result = s1.map { 
+        if (it.isUpperCase()) {
+            it.lowercaseChar()
+        } else {
+            it.uppercaseChar()
+        }
+    }.joinToString("")
+
+    println(result)
+}
