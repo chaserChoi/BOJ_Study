3c569c7db6ad58e05b7726347f2af757654bf0a7
chaserChoi <146907065+chaserChoi@users.noreply.github.com>
2026-02-10 19:38:21 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(absolutes: IntArray, signs: BooleanArray): Int {
+        var answer: Int = 0
+        for (i in absolutes.indices) {
+            answer += if (signs[i]) absolutes[i] else -absolutes[i]
+        }
+        return answer
+    }
+}
